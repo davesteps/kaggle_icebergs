eb4e90960ed0da84ac7ae20319ddd63f49c054f8
davesteps <davesteps@gmail.com>
2018-01-10 11:03:31 +0000

    sdf

--- 180109_haralick.R
+++ 180109_haralick.R
@@ -23,7 +23,7 @@ ggplot(train)+geom_density(aes(x=inc_angle,fill=factor(is_iceberg)),alpha=0.5)
 
 
 procImage <- function(i){
-  # i <- 60
+  i <- 214
   print(i)
   r1 <- raster(matrix(band_1[[i]],nrow = 75,ncol = 75))
   r1mf <- focal(r1,w=matrix(1,3,3), fun=median,pad=T, padValue= mean(band_1[[i]]))
@@ -45,8 +45,12 @@ procImage <- function(i){
     filter(layer.1>quantile(layer.1,0.99)) %>%
     select(x,y)
   clmp <- clump(r3-1)
+  
+  # if there are multiple clumps remove 
+  
+  plot(clmp)
   # max(clmp[],na.rm = T)
-  # points(q99)
+  points(q99)
   extObj <- modal(extract(clmp,q99),na.rm=T)
   clmp[clmp!=extObj] <- NA
   
@@ -56,17 +60,17 @@ procImage <- function(i){
   
   dev.off()
 
-  ft1 = computeFeatures(as.matrix(!is.na(clmp)), as.matrix(r1),xname = 'band1')
-  ft2 = computeFeatures(as.matrix(!is.na(clmp)), as.matrix(r2),xname = 'band2')
+  ft1 = computeFeatures(as.matrix(!is.na(clmp)), as.matrix(r1mf),xname = 'band1')
+  ft2 = computeFeatures(as.matrix(!is.na(clmp)), as.matrix(r2mf),xname = 'band2')
   cbind.data.frame(train[i,c(1,3,2)],ft1,ft2)
   
 }
 
 # plyr::ldply(10:20, procImage)
 featDf <- plyr::ldply(1:nrow(train), procImage)
-# saveRDS(featDf,file='featureDf.rdata')
+saveRDS(featDf,file='featureDf.rdata')
 
-featDf <- readRDS('featureDf.rdata')
+# featDf <- readRDS('featureDf.rdata')
 
 library(randomForest)
 
@@ -81,12 +85,12 @@ nrow(trainX)
 nrow(trainY)
 
 fit <- randomForest(x=trainX,y=trainY,ntree=5e3)
-
+fit
 actual <- as.integer(as.character(fit$y))
 pred <- fit$votes[,2]
 Metrics::logLoss(actual,pred)
 
-
+fit
 fit
 plot(fit)
 vi <- varImpPlot(fit)
@@ -108,13 +112,17 @@ pred <- fit$votes[,2]
 Metrics::logLoss(actual,pred)
 
 
+train[fit$predicted!=fit$y,]
 
+# poor clustering eg 259 268 334 398 431 534
 # don't use median filtered
+# inspect which ones it is getting missclassifying
 # grow clump
 # make sure it is in center
 # stats from wider image
 # scale image
 # gray levels used for features
+# compute features at different distances
 # supervised model for extracting object
 
 
