8f2995b6d8c45700dbc3e9c613a0bd5375dbd4b1
davesteps <davesteps@gmail.com>
2018-01-22 16:40:42 +0000

    process test images

--- prcoessTestImages.R
+++ prcoessTestImages.R
@@ -3,6 +3,10 @@
 
 test <- read_json("data/test.json", simplifyVector=TRUE)
 
+saveRDS(test,'data/test.feather')
+train <- readRDS('data/test.feather')
+
+str(train)
 
 band_1 <- train$band_1
 train$band_1 <- NULL
@@ -11,5 +15,25 @@ train$band_2 <- NULL
 
 train$inc_angle <- as.numeric(train$inc_angle)
 
-testFeatDf <- plyr::ldply(1:nrow(test), procImage)
+
+testFeatDf <- plyr::ldply(1:nrow(train), procImage,F)
+str(testFeatDf)
 saveRDS(testFeatDf,file='test_featureDf.rdata')
+
+
+fit
+Xtest <- select(testFeatDf,-id)
+
+pred <- predict(fit,Xtest,type='prob')
+str(pred)
+head(pred)
+select(testFeatDf,id) %>%
+  bind_cols(data.frame(pred)) %>%
+  rename(is_iceberg = X1) %>% 
+  select(-X0) %>%
+  write.csv(.,'pred1.csv',row.names = F)
+
+
+
+
+
